3fd2bdc28e2621b0fb508954264c1e46383d3756
Austin Brodeur <austinbrodeur@shaw.ca>
2019-12-06 23:24:21 -0700

    Implemented CHD model, updated report

--- CHDModel.py
+++ CHDModel.py
@@ -3,4 +3,113 @@ import functools
 
 import numpy as np
 import tensorflow as tf
+import pandas as pd
 
+train_file_path = "heart_train.csv"
+test_file_path = "heart_test.csv"
+
+LABEL_COLUMN = "chd"
+LABELS = [0, 1]
+NUMERIC_FEATURES = ['row.names', 'sbp', 'tobacco', 'ldl', 'adiposity', 'typea', 'obesity', 'alcohol', 'age']
+CATEGORIES = {
+    'famhist': ['Present', 'Absent']
+}
+
+np.set_printoptions(precision=3, suppress=True)
+
+
+class PackNumericFeatures(object):
+  def __init__(self, names):
+    self.names = names
+
+  def __call__(self, features, labels):
+    numeric_features = [features.pop(name) for name in self.names]
+    numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_features]
+    numeric_features = tf.stack(numeric_features, axis=-1)
+    features['numeric'] = numeric_features
+
+    return features, labels
+
+
+
+def get_dataset(file_path, **kwargs):
+    dataset = tf.data.experimental.make_csv_dataset(
+        file_path,
+        batch_size=5,
+        label_name=LABEL_COLUMN,
+        na_value="?",
+        num_epochs=1,
+        ignore_errors=True
+    )
+    return dataset
+
+
+def show_batch(dataset):
+  for batch, label in dataset.take(1):
+    for key, value in batch.items():
+      print("{:20s}: {}".format(key,value.numpy()))
+
+
+def make_model(preprocessing_layer):
+
+    model = tf.keras.Sequential([
+        preprocessing_layer,
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid'),
+    ])
+
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy'])
+
+    return model
+
+def normalize_numeric_data(data, mean, std):
+    # Center the data
+    return (data - mean) / std
+
+
+def main():
+    raw_train_data = get_dataset(train_file_path)
+    raw_test_data = get_dataset(test_file_path)
+
+    packed_train_data = raw_train_data.map(
+        PackNumericFeatures(NUMERIC_FEATURES))
+
+    packed_test_data = raw_test_data.map(
+        PackNumericFeatures(NUMERIC_FEATURES))
+
+    desc = pd.read_csv(train_file_path)[NUMERIC_FEATURES].describe()
+
+    MEAN = np.array(desc.T['mean'])
+    STD = np.array(desc.T['std'])
+
+    normalizer = functools.partial(normalize_numeric_data, mean=MEAN, std=STD)
+
+    numeric_column = tf.feature_column.numeric_column('numeric', normalizer_fn=normalizer,
+                                                      shape=[len(NUMERIC_FEATURES)])
+    numeric_columns = [numeric_column]
+
+    numeric_layer = tf.keras.layers.DenseFeatures(numeric_columns)
+
+    categorical_columns = []
+    for feature, vocab in CATEGORIES.items():
+        cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
+            key=feature, vocabulary_list=vocab)
+        categorical_columns.append(tf.feature_column.indicator_column(cat_col))
+
+    categorical_layer = tf.keras.layers.DenseFeatures(categorical_columns)
+    preprocessing_layer = tf.keras.layers.DenseFeatures(categorical_columns + numeric_columns)
+
+    model = make_model(preprocessing_layer)
+
+    train_data = packed_train_data.shuffle(500)
+    test_data = packed_test_data
+
+    model.fit(train_data, epochs=20, verbose=2)
+    test_loss, test_accuracy = model.evaluate(test_data)
+    print('\n\nTest Loss {}, Test Accuracy {}'.format(test_loss, test_accuracy))
+
+main()
--- README.md
+++ README.md
@@ -10,6 +10,8 @@ To improve the accuracy of this model, I did the following:
 * Changed layer activation types (relu for input layers, softmax for output).
 * Changed model optimizer to Adam, as it seems to perform better than sigamoid.
 
+I made these changes based on some quick research as to which layers are optimal for training the model to recognize images.
+
 ## Part 2 Report
 
 To get the prediction scripts working and to train the notMNIST model, I did the following:
@@ -31,3 +33,7 @@ I then added convolution layers to both models. Adding convolution layers improv
 
 Adding convolution layers broke the prediction scripts, however, I could only use predict_test reliably, as I could not get images to correctly work on my machine. Thus, I only fixed predict_test to get it to work with the new layers.
 
+
+## Part 3 Report
+
+For this part, I primarily followed the tutorial for importing CSV. I didn't have time to experiment with convolution very much, however even without convolution I was able to get the sample to 67% accuracy. It does seem to overfit on some tests I ran, but I know this could be combatted a bit by implementing some convolution.
